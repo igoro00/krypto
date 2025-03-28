8c2268b0d284742f640ca38b1b5776a4cc1ec009
251492 Bukowski Jakub <251492@edu.p.lodz.pl>
2025-03-28 23:03:08 +0100

    obsługa wprowadzania i generowania klucza głównego

--- src/generateKey.ts
+++ src/generateKey.ts
@@ -1,3 +1,11 @@
-export function generateKey(n: number = 128) {
-    
+export function generateKey(n: number = 128): Uint8Array {
+    const key = new Uint8Array(n / 8);
+    crypto.getRandomValues(key);
+    return key;  
+}
+
+export function bytesToHex(key: Uint8Array): string {
+    return Array.from(key)
+        .map(byte => byte.toString(16).padStart(2, '0'))
+        .join('');
 }
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,61 @@
 import { dragAndDrop } from './dragAndDrop'
+import { bytesToHex, generateKey } from './generateKey';
 import './style.css'
 
 dragAndDrop()
+generateKey()
+
+document.addEventListener('DOMContentLoaded', () => {
+  const generateButton = document.getElementById('generate-key');
+  const mainKey = document.getElementById('key');
+  const manualKeyInput = document.getElementById('manualKey') as HTMLInputElement;
+  
+  generateButton?.addEventListener('click', () => {
+    const selectedRadio = document.querySelector<HTMLInputElement>('input[name="keyLength"]:checked');
+    
+    if (!selectedRadio) {
+      console.error('Nie wybrano długości klucza');
+      return;
+    }
+    
+    const selectedLength = selectedRadio.value;
+    let keyHex = '';
+
+    const hasManualKey = manualKeyInput && manualKeyInput.value.trim() !== '';
+    if (hasManualKey) {
+        keyHex = manualKeyInput.value.trim();
+        console.log('Ręcznie wprowadzony klucz: ' + keyHex);
+        return
+    } else {
+        let key, roundsNumber = 10;
+        switch (selectedLength) {
+          case "128":
+            key = generateKey(128);
+            break;
+          case "192":
+            key = generateKey(192);
+            roundsNumber = 12;
+            break;
+          case "256":
+            key = generateKey(256);
+            roundsNumber = 14;
+            break;
+          default:
+            key = generateKey(128);
+        }
+        
+        console.log('Wybrana długość klucza: ' + selectedLength);
+        console.log('Ilość rund: ' + roundsNumber)
+        
+        keyHex = bytesToHex(key);
+        console.log('Klucz z generatora: ' + keyHex);
+    }
+
+    if (mainKey) {
+        mainKey.textContent = keyHex;
+        console.log('Zaktualizowano element span z kluczem: ' + keyHex);
+      } else {
+        console.error('Nie znaleziono elementu o id "key"');
+      }
+  });
+});
