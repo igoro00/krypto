c43e7b018deb3fe028beab6d198972f9109bb1f5
Jakub Bukowski <littlefellapp@gmail.com>
2025-05-05 19:52:43 +0200

    Czysta gałąź zad2

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import checker from "vite-plugin-checker"
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    checker({terminal:true, typescript: true}),
-  ],
-})
