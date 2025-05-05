4cd16f58bcf3a42e4140256cd9fc2627265c7adf
Jakub Bukowski <littlefellapp@gmail.com>
2025-05-05 19:56:07 +0200

    Baza

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import checker from "vite-plugin-checker"
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    checker({terminal:true, typescript: true}),
+  ],
+})
